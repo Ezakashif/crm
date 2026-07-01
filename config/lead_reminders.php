90189a7ce76502f128afe23c8fce7e41e7d3ceef
Uneza <ezakashif@gmail.com>
2026-07-01 18:03:27 +0500

    New commits

--- config/lead_reminders.php
+++ config/lead_reminders.php
@@ -3,5 +3,5 @@
 return [
     'enabled' => env('LEAD_FOLLOW_UP_REMINDERS_ENABLED', true),
 
-    'schedule_time' => env('LEAD_FOLLOW_UP_REMINDER_TIME', '08:00'),
+    'schedule_time' => env('LEAD_FOLLOW_UP_REMINDER_TIME', '17:42'),
 ];
